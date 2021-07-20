a7c587265b07723d0904cecd42ac5b71d9f7afeb
yaohongxiao <617146817@qq.com>
2021-07-20 17:37:43 +0800

    change：增加人脸检测并裁剪

--- YXAccordingDemo/YXCategorys/YXCategoryUIImage/UIImage+YXCategory.h
+++ YXAccordingDemo/YXCategorys/YXCategoryUIImage/UIImage+YXCategory.h
@@ -121,6 +121,13 @@ typedef NS_ENUM(NSUInteger, YXGradientDirectionType) {
                directionType:(YXGradientDirectionType)directionType;
 
 /**
+ * 人脸位置检测，并裁剪包含五官的人脸
+ * @param img 包含人脸的图片
+ */
++ (void)yxDetectingAndCuttingFaceByImg:(UIImage *)img
+                              finished:(void(^)(BOOL success, UIImage *img))finished;
+
+/**
  * 动态拉伸图片（默认所表示的方位数值为图片的数值）
  * @param imgName 图片名
  * @param tensileTop 顶部数值
